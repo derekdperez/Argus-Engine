f0b1e5ee139b91cd0f67bd3354fb6cf9caa5d49c
Derek Perez <derekdperez@gmail.com>
2026-05-07 14:46:04 -0400

    update

--- src/ArgusEngine.Workers.Spider/HttpRetryPolicies.cs
+++ src/ArgusEngine.Workers.Spider/HttpRetryPolicies.cs
@@ -1,15 +1,17 @@
 using System.Net;
 using System.Net.Sockets;
 using Polly;
-using Polly.Extensions.Http;
 
 namespace ArgusEngine.Workers.Spider;
 
 public static class HttpRetryPolicies
 {
     public static IAsyncPolicy<HttpResponseMessage> SpiderRetryPolicy() =>
-        HttpPolicyExtensions
-            .HandleTransientHttpError(exception => !IsNameResolutionFailure(exception))
+        Policy<HttpResponseMessage>
+            .Handle<HttpRequestException>(exception => !IsNameResolutionFailure(exception))
+            .Or<TaskCanceledException>()
+            .OrResult(response => response.StatusCode == HttpStatusCode.RequestTimeout)
+            .OrResult(response => (int)response.StatusCode >= 500)
             .OrResult(r => r.StatusCode == HttpStatusCode.TooManyRequests)
             .WaitAndRetryAsync(3, attempt => TimeSpan.FromMilliseconds(200 * attempt));
 
